f205b9d18012f1e4b29efeda3435a121c35199a1
AnimalO <o.loginoff@gmail.com>
2023-04-15 00:10:10 +0300

    Создал репозиторий, настройки подключения к БД, модель.

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,69 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String surename;
+    @Column
+    private int age;
+
+    public User() {
+    }
+
+    public User(int id, String name, String surename, int age) {
+        this.id = id;
+        this.name = name;
+        this.surename = surename;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurename() {
+        return surename;
+    }
+
+    public void setSurename(String surename) {
+        this.surename = surename;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surename='" + surename + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,44 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+import web.repositories.UesrRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl {
+    private final UesrRepository userRepository;
+
+    @Autowired
+    public UserService(UesrRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserServiceImpl(UesrRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void update(int id, User updatedUser) {
+        updatedUser.setId(id);
+        userRepository.save(updatedUser);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
+}
